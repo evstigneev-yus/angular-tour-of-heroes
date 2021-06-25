a8be780a5baec6915996f4cb3426518e3fa3f6b3
Yuri <yuri@legalmonkeys.com>
2021-06-25 13:37:39 +0800

    1

--- karma.conf.js
+++ karma.conf.js
@@ -37,8 +37,16 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
-    restartOnFileChange: true
+    customLaunchers: {
+      ChromiumHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+        ]
+      }
+    }
   });
 };
